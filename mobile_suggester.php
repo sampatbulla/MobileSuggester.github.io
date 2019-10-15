f1bbf2b7ac61df0a609e61cfd8b7898cf6c8f1dc
Rajat S P <48953298+RAJAT-SP@users.noreply.github.com>
2019-10-15 20:32:16 +0530

    added space

--- mobile_suggester.php
+++ mobile_suggester.php
@@ -2,7 +2,7 @@
 <html class = "mobile_suggester">
 <head>
 	<style>
-		.error {color: #FF0000;}
+		.error {color: #FF0000;} 
 	</style>
 	<title>Mobile Suggester : Find the Phone that suits your needs</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
